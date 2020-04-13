842e167eed370f281f2c0140b8a298a575e07fb7
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-04-14 00:53:05 +0200

    Aggiorna alive

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,8 +11,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "senza nome"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`🔰🔗 USERBOT 🔗🔰`**\n\n"
-                     "`➖ Vers. Telethon: 7.0.1\n➖ Vers. Python: 3.8.0\nℹ️ UPDATE:` @IOIIIOIIIOI\n"
-                     "`🔖 CREATORE BOT:` [SnapDragon7410](tg://user?id=719877937)\n"
-                     "`✅ CPU: Qualcomm Snapdragon 855+\n\n🔰🔗 DATI USER 🔗🔰\n\n`"
-                     f"`👤 USER`: {DEFAULTUSER}")
+    await alive.edit("**🔰🔗 USERBOT 🔗🔰**\n\n"
+                     "**➖ Telethon: 7.0.1**\n**➖ Python: 3.8.0**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
+                     "**ℹ️ UPDATE: @IOIIIOIIIOI\n➖ CREATORE BOT: [SnapDragon7410](tg://user?id=719877937)**\n"
+                     "**➖ CPU: Qualcomm Snapdragon 855+\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n🔰🔗 DATI USER 🔗🔰\n\n**"
+                     f"**👤 USER**: {DEFAULTUSER}")
