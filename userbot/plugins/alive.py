c2226c72ec3404d04ae94cf836e991f7d96222bf
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-04-13 09:27:33 +0200

    Add files via upload

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,4 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+"""Check if userbot alive. """
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -11,9 +11,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "senza nome"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`⚡∇⚡ STATUS USERBOT ⚡∇⚡`**\n\n"
-                     "`-Versione Telethon: 7.0.1\n-Versione Python: 3.8.0\n🔸UPDATE:` @IOIIOIIIOIIOI\n"
-                     "`🔸CREATORE BOT:` [SnapDragon7410](tg://user?id=719877937)\n"
-                     "`-CPU: In esecuzione...\n\n⚡∇⚡ DATI USER ⚡∇⚡\n`"
-                     f"`-USER`: {DEFAULTUSER}\n"
-                     "🔸[REPOSITORY USERBOT](https://github.com/IIOOIIOIIOOII/USERBOT-ITA.git)")
+    await alive.edit("**`🔰🔗 USERBOT 🔗🔰`**\n\n"
+                     "`➖ Vers. Telethon: 7.0.1\n➖ Vers. Python: 3.8.0\nℹ️ UPDATE:` @IOIIIOIIIOI\n"
+                     "`🔖 CREATORE BOT:` [SnapDragon7410](tg://user?id=719877937)\n"
+                     "`✅ CPU: Qualcomm Snapdragon 855+\n\n🔰🔗 DATI USER 🔗🔰\n\n`"
+                     f"`👤 USER`: {DEFAULTUSER}")
