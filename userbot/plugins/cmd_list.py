00d5b54836c32f7b33bb77a3de628860429d3ebd
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-04-24 00:24:20 +0200

    Rinomina da command_list a cmd_list

---
+++ userbot/plugins/cmd_list.py
@@ -0,0 +1,20 @@
+from telethon import events
+import subprocess
+import asyncio
+import time
+
+
+@command(pattern="^.cmds", outgoing=True)
+async def install(event):
+    if event.fwd_from:
+        return
+    cmd = "ls userbot/plugins"
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    o = stdout.decode()
+    _o = o.split("\n")
+    o = "\n".join(_o)
+    OUTPUT = f"**ℹ️ LISTA PLUG-IN 🔍:**\n{o}\n\n**ℹ️ SUGGERIMENTO:** __Per vedere i comandi di un plug-in, fai:-__ \n `.help <plugin name>` **senza le parentesi < > .**\n__Tutti i plug-in potrebbero non funzionare. Visita__ ~ @IOIIIOIIIOI ~ __per assistenza.__"
+    await event.edit(OUTPUT)
