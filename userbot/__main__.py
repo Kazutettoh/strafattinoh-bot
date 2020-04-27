12ff433c075b1ad5e1c870a598048cccec73deb0
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-04-28 00:47:40 +0200

    #MODIFICA IN SUDO_USERS
    fare in modo di non caricare sudo_users

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -10,6 +10,8 @@ from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from pathlib import Path
 import asyncio
 import telethon.utils
+import heroku3
+
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
@@ -23,17 +25,26 @@ if len(argv) not in (1, 3, 4):
 else:
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        print("Initiating Inline Bot")
+        print("Start Inline Bot")
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
             "TG_BOT_TOKEN",
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        print("Initialisation finished with no errors")
-        print("Starting Userbot")
+        print("Start completato no errori")
+        print("Start Userbot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("Startup Completed")
+        if Var.HEROKU_APP_NAME and Var.HEROKU_API_KEY is not None:
+            Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+            app = Heroku.app(Var.HEROKU_APP_NAME)
+            heroku_var = app.config()
+            variable = "SUDO_USERS"
+            if variable in heroku_var:
+                del heroku_var[variable]
+            else:
+                print("All Good!")
+        print("Start Completato")
     else:
         bot.start()
     
@@ -49,11 +60,9 @@ for name in files:
 
 import userbot._core
 
-print("Il tuo userbot è in esecuzione. Test con .alive")
+print("Userbot in esecuzione, test con .alive")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
-
-
