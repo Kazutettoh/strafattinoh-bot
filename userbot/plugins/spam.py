64d96d71dd73b9ce37dd176cc6966462eb61c7f7
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-04-29 01:21:06 +0200

    File si spera funzionante

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -1,44 +1,124 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.b (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
 
 import asyncio
 import time
 
-from asyncio import wait
-
+from asyncio import wait, sleep
 from telethon import events
-from userbot.utils import admin_cmd
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot.utils import register
 
 
-@borg.on(admin_cmd("spam"))
+
+@register(outgoing=True, pattern="^.spam (.*)")
 async def spammer(e):
+    counter = int(e.pattern_match.group(1).split(' ', 1)[0])
+    spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
+    await e.delete()
+    await asyncio.wait([e.respond(spam_message) for i in range(counter)])
+    if BOTLOG:
+        await e.client.send_message(BOTLOG_CHATID, "#SPAM\n"
+                                    "Spam was executed successfully")
+
+
+
+@register(outgoing=True, pattern="^.bigspam")
+async def bigspam(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
-        counter = int(message[6:8])
-        spam_message = str(e.text[8:])
-
-        await wait(
-            [e.respond(spam_message) for i in range(counter)]
-            )
+        counter = int(message[9:13])
+        spam_message = str(e.text[13:])
+        for i in range(1, counter):
+            await e.respond(spam_message)
         await e.delete()
+        if BOTLOG:
+            await e.client.send_message(
+                BOTLOG_CHATID,
+                "#BIGSPAM \n\n"
+                "Bigspam was executed successfully"
+                )
 
 
-@borg.on(admin_cmd("tspam"))
+
+@register(outgoing=True, pattern="^.cspam (.*)")
 async def tmeme(e):
-     tspam = str(e.text[7:])
-     message = tspam.replace(" ", "")
-     for letter in message:
-         await e.respond(letter)
-     await e.delete()
+    cspam = str(e.pattern_match.group(1))
+    message = cspam.replace(" ", "")
+    await e.delete()
+    for letter in message:
+        await e.respond(letter)
+    if BOTLOG:
+        await e.client.send_message(
+            BOTLOG_CHATID, "#CSPAM\n"
+            "TSpam was executed successfully")
 
 
-@borg.on(admin_cmd("wspam ?(.*)"))
-async def _(event):
-    message = event.text[10:]
-    count = int(event.text[8:10])
+
+@register(outgoing=True, pattern="^.delayspam (.*)")
+async def spammer(e):
+    spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
+    counter = int(e.pattern_match.group(1).split(' ', 2)[1])
+    spam_message = str(e.pattern_match.group(1).split(' ', 2)[2])
+    await e.delete()
+    for i in range(1, counter):
+        await e.respond(spam_message)
+        await sleep(spamDelay)
+    if BOTLOG:
+        await e.client.send_message(
+            BOTLOG_CHATID, "#DelaySPAM\n"
+            "DelaySpam was executed successfully")
+
+
+
+@register(outgoing=True, pattern="^.picspam")
+async def tiny_pic_spam(e):
+    message = e.text
+    text = message.split()
+    counter = int(text[1])
+    link = str(text[2])
+    await e.delete()
+    for i in range(1, counter):
+        await e.client.send_file(e.chat_id, link)
+    if BOTLOG:
+        await e.client.send_message(
+            BOTLOG_CHATID, "#PICSPAM\n"
+            "PicSpam was executed successfully")
+
+
+
+@register(outgoing=True, pattern="^.repeat")
+async def repeat(e):
+    message = e.text[10:]
+    count = int(e.text[8:10])
     repmessage = message * count
-    await wait([event.respond(repmessage)for i in range(count)])
-    await event.delete()
+    await e.respond(repmessage)
+    await e.delete()
+    
+
+
+@register(outgoing=True, pattern="^.repeats")
+async def repeats(e):
+    message = e.text[10:]
+    count = int(e.text[8:10])
+    repmessage = message * count
+    await wait([e.respond(repmessage)for i in range(count)])
+    await e.delete()
+
+
+
+@register(outgoing=True, pattern="^.wspam (.*)")
+async def tmeme(e):
+    wspam = str(e.pattern_match.group(1))
+    message = wspam.split()
+    await e.delete()
+    for word in message:
+        await e.respond(word)
+    if BOTLOG:
+        await e.client.send_message(
+            BOTLOG_CHATID, "#WSPAM\n"
+            "WSpam was executed successfully")
+
+            
