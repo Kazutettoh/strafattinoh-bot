ea6749222aceb293d9ba42c834e24fb5507fce08
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-04-28 18:05:47 +0200

    Add files via upload

---
+++ userbot/plugins/animazioni5.py
@@ -0,0 +1,47 @@
+"""
+Commands:
+.clock
+.moon
+.lmoon
+.smoon
+.tmoon
+"""
+
+
+import asyncio
+
+from collections import deque
+
+from telethon import events
+
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd, register
+
+
+
+@register(outgoing=True, pattern="^.clock$")
+async def _(event):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+	    if event.fwd_from:
+		    return
+	    deq = deque(list("🕙🕘🕗🕖🕕🕔🕓🕒🕑🕐🕛"))
+	    for _ in range(32):
+		    await asyncio.sleep(0.1)
+		    await event.edit("".join(deq))
+		    deq.rotate(1)
+
+
+
+@register(outgoing=True, pattern="^.moon$")
+async def _(event):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+	    if event.fwd_from:
+		    return
+	    deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+	    for _ in range(32):
+		    await asyncio.sleep(0.1)
+		    await event.edit("".join(deq))
+		    deq.rotate(1)
+
+
+            
