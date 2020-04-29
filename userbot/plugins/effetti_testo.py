5db4f19ec20604616515ebde026113fda203a173
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-04-29 19:00:51 +0200

    .mock

--- userbot/plugins/effetti_testo.py
+++ userbot/plugins/effetti_testo.py
@@ -1,6 +1,7 @@
 """
 COMMAND :
 .ae
+.mock
 .shout
 .str
 .type
@@ -45,6 +46,32 @@ async def _(event):
     text = "".join(aesthetify(text))
     await event.edit(text=text, parse_mode=None, link_preview=False)
     raise events.StopPropagation
+
+
+
+@register(outgoing=True, pattern="^.mock(?: |$)(.*)")
+async def spongemocktext(mock):
+    """ Do it and find the real fun. """
+    if not mock.text[0].isalpha() and mock.text[0] not in ("/", "#", "@", "!"):
+        reply_text = list()
+        textx = await mock.get_reply_message()
+        message = mock.pattern_match.group(1)
+        if message:
+            pass
+        elif textx:
+            message = textx.text
+        else:
+            await mock.edit("`gIvE sOMEtHInG tO MoCk!`")
+            return
+
+        for charac in message:
+            if charac.isalpha() and random.randint(0, 1):
+                to_app = charac.upper() if charac.islower() else charac.lower()
+                reply_text.append(to_app)
+            else:
+                reply_text.append(charac)
+
+        await mock.edit("".join(reply_text))
 
 
 
