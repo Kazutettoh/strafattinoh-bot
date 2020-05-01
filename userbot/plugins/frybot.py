ec9ecf9c73c1d771bebf9e351b0b38bb568e9a82
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-05-01 17:07:10 +0200

    #IMPORTA ALIVE E ELIMINA ECCESSI

--- userbot/plugins/frybot.py
+++ userbot/plugins/frybot.py
@@ -1,36 +1,39 @@
-#credits: @r4v4n4
 import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.utils import admin_cmd
+from userbot import CMD_HELP, ALIVE_NAME
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "I'M STUPID"
 
-@borg.on(admin_cmd("frybot ?(.*)"))
+
+@borg.on(admin_cmd(pattern=("frybot ?(.*)")))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a foto/sticker**")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.media:
-       await event.edit("```reply to text message```")
+       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a foto/sticker**")
        return
     chat = "@image_deepfrybot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
+       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a un user, no al bot.**")
        return
-    await event.edit("```Processing```")
-    async with borg.conversation(chat) as conv:
+    await event.edit(f"`{DEFAULTUSER}:`**Creo img/sticker...**")
+    async with event.client.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=432858024))
-              await borg.forward_messages(chat, reply_message)
+              await event.client.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+              await event.reply("```Please sblocca @image_deepfrybot ```")
               return
           if response.text.startswith("Forward"):
-              await event.edit("```can you kindly disable your forward privacy settings for good?```")
+             await event.edit(f"`{DEFAULTUSER}:`**privacy error**")
           else: 
-              await borg.send_file(event.chat_id, response.message.media)
+             await event.delete()
+             await event.send_file(event.chat_id, response.message.media)
--- userbot/plugins/stickersfoto.py
+++ userbot/plugins/stickersfoto.py
@@ -1,49 +1,34 @@
 import datetime
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-import datetime
 import asyncio
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.events import register
-from userbot import bot, CMD_HELP
-
-from userbot import bot, CMD_HELP
-import io
 import math
 import urllib.request
-from os import remove
-from userbot.events import register
 import io
 import os
 import random
 import textwrap
-from PIL import Image, ImageDraw, ImageFont
-from telethon.tl.types import InputMessagesFilterDocument
 
-from PIL import Image
-import random
-from telethon.tl.types import DocumentAttributeFilename, MessageMediaPhoto
-from userbot import bot, CMD_HELP
-import io
-import os
-import random
-import textwrap
+from os import remove
+
 from PIL import Image, ImageDraw, ImageFont
-from telethon.tl.types import InputMessagesFilterDocument
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import DocumentAttributeFilename, MessageMediaPhoto
+from telethon.tl.types import InputMessagesFilterDocument
 from telethon.tl.types import InputStickerSetID
 from telethon.tl.types import DocumentAttributeSticker
-from userbot import CMD_HELP, ALIVE_NAME
+
+from userbot.events import register
+from userbot import bot, CMD_HELP, ALIVE_NAME
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "I'M STUPID"
 
 
 @register(outgoing=True, pattern="^.ext1(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
-        return 
+        return  
     if not event.reply_to_msg_id:
        await event.edit(f"`{DEFAULTUSER}:`**Rispondi ad una foto**")
        return
@@ -72,4 +57,3 @@ async def _(event):
           		await event.edit(f"`{DEFAULTUSER}:`Please vai su @DrWebBot e select la lingua.") 
           	else: 
           			await bot.send_file(event.chat_id, response.message.media)
-
