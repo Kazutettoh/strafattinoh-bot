a55ceb62631558c96b70ca1955ad576293aa7da0
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-05-03 19:25:04 +0200

    Add files via upload

--- userbot/plugins/blacklist.py
+++ userbot/plugins/blacklist.py
@@ -18,7 +18,7 @@ from userbot import CMD_HELP
 async def on_new_message(event):
     # TODO: exempt admins from locks
     name = event.raw_text
-    snips = sql.get_chat_blacklist(event.chat_id)
+    snips = blacklist_sql.get_chat_blacklist(event.chat_id)
     for snip in snips:
         pattern = r"( |^|[^\w])" + re.escape(snip) + r"( |$|[^\w])"
         if re.search(pattern, name, flags=re.IGNORECASE):
@@ -26,7 +26,7 @@ async def on_new_message(event):
                 await event.delete()
             except Exception as e:
                 await event.reply("I do not have DELETE permission in this chat")
-                sql.rm_from_blacklist(event.chat_id, snip.lower())
+                blacklist_sql.rm_from_blacklist(event.chat_id, snip.lower())
             break
 
 
@@ -35,20 +35,20 @@ async def on_add_black_list(event):
     text = event.pattern_match.group(1)
     to_blacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
     for trigger in to_blacklist:
-        sql.add_to_blacklist(event.chat_id, trigger.lower())
+        blacklist_sql.add_to_blacklist(event.chat_id, trigger.lower())
     await event.edit("Added {} triggers to the blacklist in the current chat".format(len(to_blacklist)))
 
 
 @borg.on(admin_cmd("listblacklist"))
 async def on_view_blacklist(event):
-    all_blacklisted = sql.get_chat_blacklist(event.chat_id)
+    all_blacklisted = blacklist_sql.get_chat_blacklist(event.chat_id)
     OUT_STR = "Blacklists in the Current Chat:\n"
     if len(all_blacklisted) > 0:
         for trigger in all_blacklisted:
             OUT_STR += f"👉 {trigger} \n"
     else:
         OUT_STR = "No BlackLists. Start Saving using `.addblacklist`"
-    if len(OUT_STR) > Config.MAX_MESSAGE_SIZE_LIMIT:
+    if len(OUT_STR) > 4095:
         with io.BytesIO(str.encode(OUT_STR)) as out_file:
             out_file.name = "blacklist.text"
             await borg.send_file(
@@ -70,7 +70,7 @@ async def on_delete_blacklist(event):
     to_unblacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
     successful = 0
     for trigger in to_unblacklist:
-        if sql.rm_from_blacklist(event.chat_id, trigger.lower()):
+        if blacklist_sql.rm_from_blacklist(event.chat_id, trigger.lower()):
             successful += 1
     await event.edit(f"Removed {successful} / {len(to_unblacklist)} from the blacklist")
 
