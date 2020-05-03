195dbf845d84ef6cbe33fdc622e016804d74dd22
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-05-03 02:08:30 +0200

    #RIPRISTINATO

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -103,7 +103,6 @@ if ENV:
         YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
         GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
         #MongoDB
-        BOTLOG = os.environ.get("BOTLOG", None)
         MONGO_URI = os.environ.get("MONGO_URI", None)
         #Lydia API
         LYDIA_API = os.environ.get("LYDIA_API",None)
