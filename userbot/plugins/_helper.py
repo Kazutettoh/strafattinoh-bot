40452249235d4a19930451eef8e262e60dc17c6a
KronosXY <63565341+KronosXY@users.noreply.github.com>
2020-05-05 18:41:13 +0200

    #FIX FINALE ........

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -35,7 +35,7 @@ async def cmd_list(event):
                 await event.edit(input_str + " plugin non valido!")
         else:
             help_string = f"""Userbot Helper.. di {DEFAULTUSER} \n\n
-`Userbot Helper per i comandi`\n__Premi .help plugin_name, in caso non sia qui.__"""
+`Helper per i comandi`\n__Premi .help plugin_name, se non è qui.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
